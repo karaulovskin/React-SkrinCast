fae8ede0e59a1e1a3f0720d6e34ba5f3e0cdc3ec
karaulovskin <karaulovskin@yandex.ru>
2019-07-29 20:08:07 +0500

    add style

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Article(props) {
-    const {article} = props
-    const body = <selection>{article.text}</selection>
-    return (
-        <div>
-            <h2>{article.title}</h2>
-            {body}
-            <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
-        </div>
-    )
+class Article extends Component {
+    state = {
+        isOpen: true
+    }
+
+    render() {
+        const {article} = this.props
+        const body = this.state.isOpen && <section className="card-text">{article.text}</section>
+        return (
+            <div className="card mx-auto" style = {{width: '50%'}}>
+                <div className="card-header">
+                    <h2>
+                        {article.title}
+                        <button onClick={this.handleClick} className="btn btn-primary byn-lg float-right">
+                            {this.state.isOpen ? 'close' : 'open'}
+                        </button>
+                    </h2>
+                </div>
+                <div className="card-body">
+                    <h6 className="card-subtitle text-muted">creation date: {(new Date(article.date)).toDateString()}</h6>
+                    {body}
+                </div>
+            </div>
+        )
+    }
+
+    handleClick = () => {
+        console.log('_click!');
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
 }
 
 export default Article
